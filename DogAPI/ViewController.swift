ece9569dcfdff05f23f8447bf56d80f14cbcf44d
Dhaval Nimavat <dr.dhaval.nimavat@gmail.com>
2022-09-27 14:08:57 +0530

    Print Command

--- DogAPI/ViewController.swift
+++ DogAPI/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    @IBOutlet weak var myImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeImage(_ sender: Any) {
+        dogs()
+    }
+    
+    
+    func dogs()
+    {
+        //Define URL for json
+        let myURL = URL(string: "https://dog.ceo/api/breeds/image/random")
+        //Convert URL into URLRequest
+        var myRequest = URLRequest(url: myURL!)
+        //Define the method of request
+        myRequest.httpMethod = "GET"
+        
+        //Task Create
+        let task = try! URLSession.shared.dataTask(with: myRequest)
+        { (mydata,URLResponse,error) in
+            let jsonData = try! JSONSerialization.jsonObject(with: mydata!) as! [String:Any]
+            print("WebSite Data",jsonData)
+            let messsagedata = jsonData["message"] as! String
+            print("Dog URL",messsagedata)
+            let statusSTring = jsonData["status"] as! String
+            //Set image to imageview
+            DispatchQueue.main.async {
+                //URL - img DAta -- imageview
+                let dogurl = URL(string: messsagedata)
+                let imgData = try! Data(contentsOf: dogurl!)
+                self.myImage.image = UIImage(data: imgData)
+            }
+        }
+        print("All Set")
+        //Task Execute
+        task.resume()
+    }
+    
 }
 
